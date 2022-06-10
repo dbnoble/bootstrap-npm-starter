a7e75ba069bc0f8376efaaef3ce59041014fec51
dbnoble <brennen@vertical3.com>
2022-06-10 12:44:53 -0500

    First update

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -20,3 +20,64 @@
 
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
+
+ var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+ var alphabetArray = alphabet.split('');
+
+$(document).ready(function() {
+
+$.wait = function( callback, seconds){
+   return window.setTimeout( callback, seconds * 1000 );
+}
+
+  	function isLetter(c) {
+  		return c.toLowerCase() != c.toUpperCase();
+	}
+    
+  function wagdieText(input) {
+    var result = '';
+    $.each(input.split(''), function() {
+     if(isLetter(this)){
+      var calcNum = alphabetArray.indexOf(this[0]) + 120172;
+      result += '&#' + calcNum + ';';
+	}
+	else
+		{
+			result += this;
+		}
+    });
+    return result;
+  }
+
+    var initialText = $('#wagdieField').val();
+    $('#wagdieResult').html(wagdieText(initialText));
+
+function copyToClipboard(text) {
+    var sampleTextarea = document.createElement("textarea");
+    document.body.appendChild(sampleTextarea);
+    sampleTextarea.value = text; //save main text in it
+    sampleTextarea.select(); //select textarea contenrs
+    document.execCommand("copy");
+    document.body.removeChild(sampleTextarea);
+}
+function copyButton(){
+    copyToClipboard($('#wagdieResult').text());
+}
+  
+  $('#wagdieButton').click(function() {
+    var theText = $('#wagdieField').val();
+    $('#wagdieResult').html(wagdieText(theText));
+    $('#resultsDiv button').attr('disabled', false);
+  });
+  
+  $('#tweetThis').click(function() {
+    window.open('https://twitter.com/intent/tweet?text='+$('#wagdieResult').text());
+  });
+  
+  $('#copyToClipboard').click(function() {
+    copyButton();
+    $(this).html('Copied!');
+    $.wait( function(){ $('#copyToClipboard').html('Copy to Clipboard') }, 1);
+  });
+  
+});
